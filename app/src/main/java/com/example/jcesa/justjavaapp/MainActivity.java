aeb22641f5d5d71bd3f87fff640ed2423595b5dd
yulio94 <yulio94@users.noreply.github.com>
2018-11-02 00:18:54 -0600

    All class changes 5 and 6.

--- app/src/main/java/com/example/jcesa/justjavaapp/MainActivity.java
+++ app/src/main/java/com/example/jcesa/justjavaapp/MainActivity.java
@@ -11,12 +11,11 @@ package com.example.jcesa.justjavaapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import com.example.jcesa.justjavaapp.R;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -31,14 +30,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public String getNameOfClient() {
+        EditText getNameEditTExt = findViewById(R.id.client_name);
+        getNameEditTExt.setAutofillHints(View.AUTOFILL_HINT_NAME);
+        return getNameEditTExt.getText().toString();
+    }
+
+    /**
+     * This method show a resume message
+     *
+     * @param price           of the order
+     * @param hasChocolate    of the order
+     * @param addWhippedCream is whether or not  the user wants whipped cream topping
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean hasChocolate) {
+        String priceMessage = "Name: " + getNameOfClient();
+        priceMessage += "\nAdd Whipped Cream? " + addWhippedCream;
+        priceMessage += "\nAdd Chocolate? " + hasChocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: " + "$" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
         int price = calculatePrice();
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank you!";
-        displayPrice(priceMessage);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
     }
 
     /**
@@ -71,16 +97,16 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(String.valueOf(number));
     }
 
     /**
      * This method displays the given price on the screen.
      *
-     * @param textPrice this text is the price of coffees.
+     * @param message this text is the price of coffees.
      */
-    private void displayPrice(String textPrice) {
-        TextView priceTextView = findViewById(R.id.price_text_view);
-        priceTextView.setText(textPrice);
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
