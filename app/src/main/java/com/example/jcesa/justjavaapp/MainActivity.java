7fc3c9536e0c2ca78eab010cd6cb4e4a35608b57
yulio94 <yulio94@users.noreply.github.com>
2018-11-19 23:05:00 -0600

    All class changes 5 and 6.

--- app/src/main/java/com/example/jcesa/justjavaapp/MainActivity.java
+++ app/src/main/java/com/example/jcesa/justjavaapp/MainActivity.java
@@ -7,12 +7,15 @@
  */
 package com.example.jcesa.justjavaapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -38,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
      * @return text summary
      */
     private String createOrderSummary(int price, boolean addWhippedCream, boolean hasChocolate, String hasNameClient) {
-        String priceMessage = "Name: " + hasNameClient;
-        priceMessage += "\nAdd Whipped Cream? " + addWhippedCream;
-        priceMessage += "\nAdd Chocolate? " + hasChocolate;
-        priceMessage += "\nQuantity: " + quantity;
+        String priceMessage = getString(R.string.your_name) + " " + hasNameClient;
+        priceMessage += "\n" + getString(R.string.add_whipped_cream) + " " + addWhippedCream;
+        priceMessage += "\n" + getString(R.string.text_chocolate) + " " + hasChocolate;
+        priceMessage += "\n" + getString(R.string.text_cantidad) + " " + quantity;
         priceMessage += "\nTotal: " + "$" + price;
-        priceMessage += "\nThank you!";
+        priceMessage += "\n" + getString(R.string.thank_you) + " ";
         return priceMessage;
     }
 
@@ -51,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+
         /* get name of client*/
         EditText getNameEditTExt = findViewById(R.id.client_name);
-        getNameEditTExt.setAutofillHints(View.AUTOFILL_HINT_NAME);
         String hasNameClient = getNameEditTExt.getText().toString();
         /* if user wants whipped Cream */
         CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
@@ -61,24 +64,49 @@ public class MainActivity extends AppCompatActivity {
         /* if user wants chocolate */
         CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
-        int price = calculatePrice();
+
+        int price = calculatePrice(hasChocolate, hasWhippedCream);
         String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, hasNameClient);
-        displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, "");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Check List");
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * Calculates the price of the order.
      *
+     * @param hasChocolate
+     * @param hasWhippedCream
      * @return total price
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean hasChocolate, boolean hasWhippedCream) {
+        int price = 5;
+
+        if (hasWhippedCream) {
+            price = price + 1;
+        }
+        if (hasChocolate) {
+            price = price + 2;
+        }
+        return quantity * price;
     }
 
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            //Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffee", Toast.LENGTH_SHORT).show();
+            //Exit this method early.
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
     }
@@ -87,6 +115,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            //Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            //Exit this method early.
+            return;
+        }
         quantity = quantity - 1;
         display(quantity);
     }
@@ -99,13 +133,13 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText(String.valueOf(number));
     }
 
-    /**
-     * This method displays the given price on the screen.
-     *
-     * @param message this text is the price of coffees.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+//    /**
+//     * This method displays the given price on the screen.
+//     *
+//     * @param message this text is the price of coffees.
+//     */
+////    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
 }
